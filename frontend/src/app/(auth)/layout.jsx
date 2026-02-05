f469a0e0d040fa200998e616a1743a5b142e4e0d
RAHTAB\rahta <arminfaa@gmail.com>
2026-02-05 11:48:06 +0330

    style changes

--- frontend/src/app/(auth)/layout.jsx
+++ frontend/src/app/(auth)/layout.jsx
@@ -1,7 +1,7 @@
 function Layout({ children }) {
   return (
     <div className="flex items-center justify-center w-full">
-      <div className="w-full max-w-md p-2">{children}</div>
+      <div className="w-full max-w-md px-6 lg:py-2 lg:px-2">{children}</div>
     </div>
   );
 }
